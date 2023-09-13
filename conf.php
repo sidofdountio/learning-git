f18f936e0fe0e298e0d713945b9834b2d60e72d0
sidofdountio <sidofdountio406@gmail.com>
2023-09-13 23:18:40 +0100

    first commit on work branch

--- conf.php
+++ conf.php
@@ -1,2 +1,10 @@
-<?php 
+<?php
 // TODO: getConnection(), isLog(), logUser($username,$password);
+
+function isLog(): bool
+{
+    if (isset($_SESSION["userEmail"]))
+
+        return true;
+    return false;
+}
